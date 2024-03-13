db30679ba14840980d05d38d8fc8fa430fc5ea44
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2024-03-13 16:49:14 -0400

    Created a swtich statement for playround but its buggy need to fix it

--- code.js
+++ code.js
@@ -1,18 +1,54 @@
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice = '';
     if(randomNum === 0){
-        computerChoice = "Rock"
+        computerChoice = "rock"
     }
     else if(randomNum === 1){
-        computerChoice = "Paper"
+        computerChoice = "paper"
     }
     else{
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
     }
-    console.log(computerChoice);
 }
 
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
+function playRound(playerSelection,computerSelection){
+    playerSelection.toLowerCase();
+    switch(playerSelection,computerSelection){
+        case playerSelection === "rock" :
+            if(computerSelection === "rock"){
+                console.log("Its a Tie! Both Players used Rock")
+            }
+            else if(computerSelection === "paper"){
+                console.log("You Lose! Paper beats Rock");
+            }
+            else if (computerSelection === "scissors"){
+                console.log("You Win! Rock beats Scissors");
+            }
+       case playerSelection === "paper" :
+            if(computerSelection === "rock"){
+                console.log("You Win! Paper beats Rock")
+            }
+            else if(computerSelection === "paper"){
+                console.log("Its a Tie! Both Players used Paper");
+            }
+            else if (computerSelection === "scissors"){
+                console.log("You Lose! Scissors beats Paper");
+            }
+        case playerSelection === "scissors" :
+            if(computerSelection === "rock"){
+                console.log("You Lose! Rock beats Scissors")
+                }
+            else if(computerSelection === "paper"){
+                console.log("You Win! Scissors beats Paper");
+            }
+            else if (computerSelection === "scissors"){
+                console.log("Its a Tie! Both Players used Scissors");
+            }
+    }
+
+    playRound(playerSelection, computerSelection);
+}
