88780f9f9d2b5365c133760c8c7f24db1388675f
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2024-03-14 22:16:25 -0400

    computer selection returns the values rock paper or scissors randomly.

--- code.js
+++ code.js
@@ -1,54 +1,64 @@
 let playerSelection = "rock";
-let computerSelection = getComputerChoice();
+let computerSelection = "";
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
-    let computerChoice = '';
     if(randomNum === 0){
-        computerChoice = "rock"
+        return computerSelection = "rock"
     }
     else if(randomNum === 1){
-        computerChoice = "paper"
+        return computerSelection = "paper"
     }
     else{
-        computerChoice = "scissors"
+        return computerSelection = "scissors"
     }
 }
 
 function playRound(playerSelection,computerSelection){
-    playerSelection.toLowerCase();
-    switch(playerSelection,computerSelection){
-        case playerSelection === "rock" :
-            if(computerSelection === "rock"){
-                console.log("Its a Tie! Both Players used Rock")
-            }
-            else if(computerSelection === "paper"){
-                console.log("You Lose! Paper beats Rock");
-            }
-            else if (computerSelection === "scissors"){
-                console.log("You Win! Rock beats Scissors");
-            }
-       case playerSelection === "paper" :
-            if(computerSelection === "rock"){
-                console.log("You Win! Paper beats Rock")
-            }
-            else if(computerSelection === "paper"){
-                console.log("Its a Tie! Both Players used Paper");
-            }
-            else if (computerSelection === "scissors"){
-                console.log("You Lose! Scissors beats Paper");
-            }
-        case playerSelection === "scissors" :
-            if(computerSelection === "rock"){
-                console.log("You Lose! Rock beats Scissors")
-                }
-            else if(computerSelection === "paper"){
-                console.log("You Win! Scissors beats Paper");
-            }
-            else if (computerSelection === "scissors"){
-                console.log("Its a Tie! Both Players used Scissors");
-            }
-    }
-
-    playRound(playerSelection, computerSelection);
+    console.log(playerSelection);
+    console.log(computerSelection);
 }
+
+
+playRound(playerSelection,getComputerChoice());
+
+
+
+
+// function playRound(playerSelection,computerSelection){
+//     playerSelection.toLowerCase();
+//     switch(playerSelection,computerSelection){
+//         case playerSelection === "rock" :
+//             if(computerSelection === "rock"){
+//                 console.log("Its a Tie! Both Players used Rock")
+//             }
+//             else if(computerSelection === "paper"){
+//                 console.log("You Lose! Paper beats Rock");
+//             }
+//             else if (computerSelection === "scissors"){
+//                 console.log("You Win! Rock beats Scissors");
+//             }
+//        case playerSelection === "paper" :
+//             if(computerSelection === "rock"){
+//                 console.log("You Win! Paper beats Rock")
+//             }
+//             else if(computerSelection === "paper"){
+//                 console.log("Its a Tie! Both Players used Paper");
+//             }
+//             else if (computerSelection === "scissors"){
+//                 console.log("You Lose! Scissors beats Paper");
+//             }
+//         case playerSelection === "scissors" :
+//             if(computerSelection === "rock"){
+//                 console.log("You Lose! Rock beats Scissors")
+//                 }
+//             else if(computerSelection === "paper"){
+//                 console.log("You Win! Scissors beats Paper");
+//             }
+//             else if (computerSelection === "scissors"){
+//                 console.log("Its a Tie! Both Players used Scissors");
+//             }
+//     }
+
+//     playRound(playerSelection, computerSelection);
+// }
